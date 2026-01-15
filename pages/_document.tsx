13ea03ecb22fac7df9f7f52b8cc7fbfdb4b02e70
y4nnr <inbox.yann@gmail.com>
2026-01-15 19:33:53 +0100

    Add proper Open Graph image and optimized favicons
    - Created og-image.png (1200x630px, 288KB) meeting Meta/Facebook requirements
    - Created optimized favicon files (16x16 and 32x32) from logo
    - Updated favicon.ico to use optimized 32x32 version (1.3KB instead of 1.5MB)
    - Updated Open Graph metadata to use proper og-image.png with correct dimensions
    - Fixed OG image dimensions to match actual file size (1200x630)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,23 +10,22 @@ export default function Document() {
       <Head>
         <meta name="description" content={description} />
         
-        {/* Favicon - Using logo.png as primary favicon with timestamp to bypass cache */}
+        {/* Favicon - Using optimized favicon files */}
         <link rel="shortcut icon" href="/favicon.ico?t=20250115" type="image/x-icon" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png?t=20250115" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png?t=20250115" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/logo.png?t=20250115" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/logo.png?t=20250115" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png?t=20250115" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png?t=20250115" />
         <link rel="apple-touch-icon" sizes="180x180" href="/logo.png?t=20250115" />
-        <meta name="msapplication-TileImage" content="/logo.png?t=20250115" />
+        <meta name="msapplication-TileImage" content="/favicon-32.png?t=20250115" />
         
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content={siteUrl} />
         <meta property="og:title" content={siteName} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={`${siteUrl}/logo.png`} />
+        <meta property="og:image" content={`${siteUrl}/og-image.png?v=20250115`} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:alt" content={`${siteName} - Logo`} />
         <meta property="og:site_name" content={siteName} />
         <meta property="og:locale" content="fr_FR" />
@@ -36,7 +35,7 @@ export default function Document() {
         <meta name="twitter:url" content={siteUrl} />
         <meta name="twitter:title" content={siteName} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={`${siteUrl}/logo.png`} />
+        <meta name="twitter:image" content={`${siteUrl}/og-image.png?v=20250115`} />
         <meta name="twitter:image:alt" content={`${siteName} - Logo`} />
       </Head>
       <body className="antialiased">
